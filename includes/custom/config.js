6e0fde2cf9b3a229e9cadca90d31f24308324716
Krisztian Korpa <krisz@Krisz_ThinkPad.localdomain>
2024-12-02 12:40:45 +0100

    fix: gpl 3 license text

--- includes/custom/config.js
+++ includes/custom/config.js
@@ -5,6 +5,17 @@
     Johan van de Werken, Krisztián Korpa,
     Simon Breton
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License in the LICENSE.txt file for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 // do not remove this line
