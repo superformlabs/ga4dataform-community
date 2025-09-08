d85e2312d3ff3cd3c47212ef383a21b081defb6b
trishdhotkar <trisha.dhotkar@infotrustllc.com>
2025-09-08 11:09:33 -0400

    Revert "Merge pull request #12 from Infotrust-llc/refactor-update_Includes_folder"
    This reverts commit 3f7603729ccb8ed47951ba3f7204b6ad00af6bc4, reversing
    changes made to 0b94913f9b3ba92734b14d520a523dcc8420a6b6.

--- includes/helper_functions.js
+++ includes/helper_functions.js
@@ -1042,41 +1042,6 @@ const generateExportDataSQL = (gcs_folder, table_ref, file_name) => {
 
 
 module.exports = {
-<<<<<<< HEAD
- // helpers
-  checkColumnNames,
-  generateParamsSQL,
-  generateURLParamsSQL,
-  generateParamSQL,
-  generateStructSQL,
-  generateListSQL,
-  generateFilterTypeFromListSQL,
-  generateArrayAggSQL,
-  generateTrafficSourceSQL,
-  generateClickIdTrafficSourceSQL,
-  getSqlUnionAllFromRowsSQL,
-  getDefaultChannelGroupingSQL,
-  urlDecodeSQL,
-  safeCastSQL,
-  clearURLSQL,
-  lowerSQL,
-  getClickIdsDimensionsSQL,
-  getConfig,
-  generateClickIdCoalesceSQL,
-  generateClickIdCasesSQL,
-  generateTransactionsDedupeSQL,
-  getEventParamKeysArray,
-  storageLabels,
-  executionLabels,
-  storageUpdateLabels,
-  generateAlterTableStatements,
-  getSqlSelectEventsAsMetrics,
-  getSqlPivotEventParams,
-  generateParamsSQL_fake,
-  getWeekAndYear,
-  getMonthAndYear,
-  getPagePathFromFullUrl
-=======
     // helpers
     checkColumnNames,
     generateParamsSQL,
@@ -1111,5 +1076,4 @@ module.exports = {
     getPagePathFromFullUrl,
     generateParamSQL,
     generateExportDataSQL
->>>>>>> refs/heads/main
 };
